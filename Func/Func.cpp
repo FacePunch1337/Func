54deb1bb10434a9f8200e1063701ac310a2803f8
FacePunch1337 <rezol1337@gmail.com>
2021-11-02 20:55:44 +0300

    Добавьте файлы проекта.

---
+++ Func/Func.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class Func {
+private: 
+    int count;
+public:
+    Func():count {0}{count = count;};
+    void operator()(int count) {
+        count++;
+        printf("Func (%p) works. Count: %d\n", this, count);
+        
+    }
+    
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    Func func1, func2;
+    func1(10);
+    func2(20);
+    func1(11);
+    func1(12);
+    func2(21);
+
+}
+
+
